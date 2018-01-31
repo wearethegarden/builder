6c524ddef612c7aa5edbd1c42e7b661f21789b6d
Jake Neal <jake@wearethegarden.co.uk>
2018-01-31 14:44:56 +0000

    Use `src` path if `watch` path isn't defined

--- src/tasks/watch.js
+++ src/tasks/watch.js
@@ -13,31 +13,31 @@ export default (config, gulp) => {
     return () => {
 
         if (config.copy && gulp.hasTask('copy')) {
-            watch(config.copy.watch, batch((events, done) => {
+            watch(config.copy.watch || config.copy.src, batch((events, done) => {
                 return sequence('copy', done);
             }));
         }
 
         if (config.css && gulp.hasTask('css')) {
-            watch(config.css.watch, batch((events, done) => {
+            watch(config.css.watch || config.css.src, batch((events, done) => {
                 return sequence('css', done);
             }));
         }
 
         if (config.img && gulp.hasTask('img')) {
-            watch(config.img.watch, batch((events, done) => {
+            watch(config.img.watch || config.img.src, batch((events, done) => {
                 return sequence('img', done);
             }));
         }
 
         if (config.js && config.js.webpack && gulp.hasTask('js:webpack')) {
-            watch(config.js.webpack.watch, batch((events, done) => {
+            watch(config.js.webpack.watch || config.js.webpack.src, batch((events, done) => {
                 return sequence('js:webpack', done);
             }));
         }
 
         if (config.lint && config.lint.php && gulp.hasTask('lint:php')) {
-            watch(config.lint.php.watch, batch((events, done) => {
+            watch(config.lint.php.watch || config.lint.php.src, batch((events, done) => {
                 return sequence('lint:php', done);
             }));
         }
